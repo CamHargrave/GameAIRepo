1c0287180ddd241815ac2c97cf4f43c91b25f75f
CamHargrave <tvastacarious@gmail.com>
2019-04-21 19:09:51 -0500

    Commit
    Commit

--- Ocean/Assets/Scripts/TutorialScripts/Heap.cs
+++ Ocean/Assets/Scripts/TutorialScripts/Heap.cs
@@ -21,6 +21,65 @@ public class Heap<T> where T : IHeapItem<T>
         currentItemCount++;
     }
 
+    public T RemoveFirst()
+    {
+        T firstItem = items[0];
+        currentItemCount--;
+        items[0] = items[currentItemCount];
+        items[0].HeapIndex = 0;
+        SortDown(items[0]);
+        return firstItem;
+    }
+
+    public void UpdateItem(T item)
+    {
+        SortUp(item);
+    }
+
+    public int Count { get { return currentItemCount; } }
+
+    public bool Contains(T item)
+    {
+        return Equals(items[item.HeapIndex], item);
+    }
+
+    void SortDown(T item)
+    {
+        while (true)
+        {
+            int childIndexLeft = item.HeapIndex * 2 + 1;
+            int childIndexRight = item.HeapIndex * 2 + 2;
+            int swapIndex = 0;
+
+            if(childIndexLeft < currentItemCount)
+            {
+                swapIndex = childIndexLeft;
+
+                if(childIndexRight < currentItemCount)
+                {
+                    if(items[childIndexLeft].CompareTo(items[childIndexRight]) < 0)
+                    {
+                        swapIndex = childIndexRight;
+                    }
+                }
+
+                if(item.CompareTo(items[swapIndex]) < 0)
+                {
+                    Swap(item, items[swapIndex]);
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            else
+            {
+                return;
+            }
+        }
+    }
+
     void SortUp(T item)
     {
         int parentIndex = (item.HeapIndex-1)/2;
@@ -30,8 +89,13 @@ public class Heap<T> where T : IHeapItem<T>
             T parentItem = items[parentIndex];
             if (item.CompareTo(parentItem) > 0)
             {
-
+                Swap(item, parentItem);
             }
+            else
+            {
+                break;
+            }
+            parentIndex = (item.HeapIndex - 1) / 2;
         }
     }
 
